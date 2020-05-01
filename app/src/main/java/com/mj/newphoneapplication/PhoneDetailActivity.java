0c32a90cfbd0184fd7e2ed7c4d5e7704fe35e4fe
김명진 <eoqrkskfk94@naver.com>
2020-05-01 22:27:29 +0900

    Merge branch 'master' of C:\Users\eoqkr\Desktop\NewPhoneApplication with conflicts.

---
+++ app/src/main/java/com/mj/newphoneapplication/PhoneDetailActivity.java
@@ -0,0 +1,14 @@
+package com;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mj.newphoneapplication.R;
+
+public class PhoneDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_detail);
+    }
+}
